6d46acffed4d49a35e9e7bd738d15222992c00d2
wallacecezagsja <wallacecezagsja@gmail.com>
2025-09-25 22:31:49 -0300

    Atualizar o AnimalRepository.java

--- projeto petshop java/main/java/br/com/petshop/projeto/repositories/AnimalRepository.java
+++ projeto petshop java/main/java/br/com/petshop/projeto/repositories/AnimalRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AnimalRepository {
-    Animal save(Animal animal); // create or update (if id exists)
-    List<Animal> findAll(); // unsorted or sorted by service
+    Animal save(Animal animal);
+
+    List<Animal> findAll();
+
     Optional<Animal> findById(int id);
     List<Animal> findByNome(String nome);
     boolean deleteById(int id);
